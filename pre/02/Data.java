ec9acc640ef2b744e3174b82bf99be908c388c06
Alif Rahman <76083827+Alif-J-Rahman@users.noreply.github.com>
2021-09-28 00:30:31 -0400

    Add files via upload

---
+++ pre/02/Data.java
@@ -0,0 +1,8 @@
+public class Data {
+   public static void main(String[] args) {
+     int intval = 5;
+     double doubval = 123.45;
+     System.out.println(intval);
+     System.out.println(doubval);
+   }
+}
