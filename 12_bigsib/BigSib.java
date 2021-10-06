7415b2c8c9f0e080b8d6dfe43baceafafba1da2c
Alif Rahman <76083827+Alif-J-Rahman@users.noreply.github.com>
2021-10-06 07:06:39 -0400

    Create BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,10 @@
+public class BigSib {
+  public String greet(String name){
+        String value = (helloMsg + " " +  name);
+        return(value);
+	}
+  String helloMsg;
+  public void setHelloMsg(String msg){
+	helloMsg = msg;
+	}
+}
