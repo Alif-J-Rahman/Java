e14742939736211c07fcceac778a8c76a1a46037
Alif Rahman <76083827+Alif-J-Rahman@users.noreply.github.com>
2021-10-06 20:42:54 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,26 +1,15 @@
-// Team With A Vowel: Lior (toothless), Alif (Grippy), Josiah (Hedwig)
-// APCS
-// HW12 -- On Elder Individuality and the Elimination of Radio Fuzz/instance variables (?)/Accessing/"editing" BigSib by making a "copy"
-// 2021-10-06
-
 /*
-Discoveries
--static isn't necessary for a function to work
--If a method is static, everything defined inside it is static, and everything used inside it MUST be static
--variables are method specific
--you can access variables by ClassName.MethodName.VariableName
--when we create a method containing helloMsg, an instance variable, we received an error if we us$
--you can define a variable without a method. This was similar to global variables in NetLogo and $
-QCC
--What is static?
--What does BigSib mean as a "variable" (in reference to richard) type?
--What is richard?
--What is an instance variable?
--What really is an object
--Can anything take the place of "static" in the method header?
--Was it possible to code this homework without removing "static" from the method header?
+Excellent Apples: Ariella Katz, Tom, Alif Rahman, Grippy
+APCS
+HW13 -- Constructor methods
 */
 
+/*
+DISCO:
+Constructor methods let us choose what the default value of an instance variable is.
+QCC:
+How does the computer know to invoke the constructor method whenever a new instance of BigSib is created?
+*/
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
