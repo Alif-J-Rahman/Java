325b593a8a4548a8c363c6381702447498eefdea
Alif Rahman <76083827+Alif-J-Rahman@users.noreply.github.com>
2021-10-06 22:01:24 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -18,10 +18,6 @@ public class BigSib {
   helloMsg = msg; //sets "Word up" as the default helloMsg whenever there is a new instance of BigSib
  }
 
- public void setHelloMsg(String msg) { //note that neither this method nor the one below is static
-  helloMsg = msg; //sets helloMsg to whatever msg is inputted as the argument to method setHelloMsg
- }
-
  public String greet(String name) { //not static because it uses an instance variable
   return(helloMsg + " " + name); //this output will be printed by Greet.java
  }
