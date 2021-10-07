e0a9d96868c9a017547226241d34bd0410d80438
Alif Rahman <76083827+Alif-J-Rahman@users.noreply.github.com>
2021-10-06 21:22:16 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -10,33 +10,37 @@ Constructor methods let us choose what the default value of an instance variable
 QCC:
 How does the computer know to invoke the constructor method whenever a new instance of BigSib is created?
 */
-public class Greet {
-  public static void main( String[] args ) {
-    String greeting;
 
-    BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
+public class Greet{
+ public static void main(String[] args) {
+  String greeting;
+  String greeting1;
+  String greeting2;
+  String greeting3;
 
-    greeting = richard.greet("freshman");
-    System.out.println(greeting);
+  BigSib richard = new BigSib();
+//richard.setHelloMsg("Word up"); we don't need this because of constructor method added to BigSib.java
 
-    BigSib spaceman = new BigSib();
-    spaceman.setHelloMsg("Salutations");
+  greeting = richard.greet("freshman");
+  System.out.println(greeting);
 
-    greeting = spaceman.greet("Dr. Spaceman");
-    System.out.println(greeting);
+  BigSib robert = new BigSib();
+  robert.setHelloMsg("Salutations");
 
-    BigSib fooey = new BigSib();
-    fooey.setHelloMsg("Hey ya");
+  greeting1 = robert.greet("Dr. Spaceman");
+  System.out.println(greeting1);
 
-    greeting = fooey.greet("Kong Fooey");
-    System.out.println(greeting);
+  BigSib raymond = new BigSib();
+  raymond.setHelloMsg("Hey ya");
 
-    BigSib mom = new BigSib();
-    mom.setHelloMsg("Sup");
+  greeting2 = raymond.greet("Kong Fooey");
+  System.out.println(greeting2);
 
-    greeting = mom.greet("mom");
-    System.out.println(greeting);
+  BigSib russel = new BigSib();
+  russel.setHelloMsg("Sup");
 
-  }
+  greeting3 = russel.greet("mom");
+  System.out.println(greeting3);
+
+ }
 }
