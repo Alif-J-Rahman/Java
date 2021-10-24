f381a7a36e7068f40ec7de3c45364d48d00dcfbb
Alif Rahman <76083827+Alif-J-Rahman@users.noreply.github.com>
2021-10-24 18:09:20 -0400

    Update Driver.java

--- 23_Coin/Driver.java
+++ 23_Coin/Driver.java
@@ -1,24 +1,15 @@
 /***
-    TNPG: NAW Fam: Alif Rahman, Nakib Abedin
-    APCS Period 06
-    HW23 -- What does equality look like?
-    2021-10-24
-    Time Spent: 1.5 hour
-    DISCO
-    - You can use an instance of a class as a parameter.
-    QCC
-    - How do we make this code work with bias as a public variable.
-   
-    driver for class Coin
-    ~~~ SUGGESTED WORKFLOW: ~~~
-    1. Compile this file and run. Note anything notable.
-    2. Move the "TOP" line down, so that it is below the first statement.
-    (emacs: with cursor at beginning of TOP line, C-k C-k, DOWN, DOWN, C-y)
-    (your editor: ???)
-    3. Compile and run again.
-    4. Resolve errors one at a time until it works.
-    5. Repeat 2-4 until TOP meets BOTTOM.
-***/
+ TNPG: NAW Fam: Alif Rahman, Nakib Abedin
+ APCS Period 06
+ HW23 -- What does equality look like?
+ 2021-10-24
+ Time Spent: 1.5 hour
+ DISCOVERIES:
+ 1) You can have an object as an argument for a method
+ Questions:
+ 1) Did we use bias correctly for the flip() method?
+ 2) Did we code the reset() method correctly?
+ ***/
 
 public class Driver {
 
@@ -48,21 +39,8 @@ public class Driver {
       else {
         System.out.println( "No match. Firestarter you can not be." );
       }
-      // bias is 0.0 since we did not define it. Hence, they will always match.
-      System.out.println("Now with bias = 0.5...");
-      yours.bias = 0.5;
-      wayne.bias = 0.5;
-      yours.flip();
-      wayne.flip();
-      System.out.println("yours: " + yours);
-      System.out.println("wayne: " + wayne);
-      // tests equals() method with bias as 0.5
-      if ( yours.equals(wayne) ) {
-        System.out.println( "Matchee matchee!" );
-      }
-      else {
-        System.out.println( "No match. Firestarter you can not be." );
-      }
+
+
 
   }//end main()
 
