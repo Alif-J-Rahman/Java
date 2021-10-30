9db9897fd7c12a548f3d1c4c6409b720f1b539e5
Alif Rahman <alifrahman2023@gmail.com>
2021-10-30 14:18:40 -0400

    init

---
+++ cb/string-2/catDog/Main.java
@@ -0,0 +1,17 @@
+// https://codingbat.com/prob/p111624
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(catDog("catdog")); // true
+        System.out.println(catDog("catcat")); // false
+        System.out.println(catDog("1cat1cadodog")); // true
+    }
+
+    /*
+     * Return true if the string "cat" and "dog" appear the same number of times
+     * in the given string.
+     */
+    public static boolean catDog(String str) {
+        
+    }
+}
