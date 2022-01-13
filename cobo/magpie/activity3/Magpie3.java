c2d4832959220a7c1977109c243253c496a06296
Alif Rahman <alifrahman2023@gmail.com>
2022-01-12 20:22:24 -0500

    finsihed

--- cobo/magpie/activity3/Magpie3.java
+++ cobo/magpie/activity3/Magpie3.java
@@ -1,3 +1,7 @@
+// Team Aliens Arriving Naughtily: Nakib Abedin + Ameer Alnasser + Alif Rahman
+// APCS pd06
+// HW56: Turing Training Wheels
+// 2022-1-12
 /**
  * A program to carry on conversations with a human user.
  * This version:
