116cdcddddada6417da4ac6cfc04866d239bb96e
Alif Rahman <alifrahman2023@gmail.com>
2022-02-14 22:48:14 -0500

    finish

---
+++ cb/recursion-1/powerN/Main.java
@@ -0,0 +1,23 @@
+// https://codingbat.com/prob/p158888
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(powerN(3,1)); // 3
+        System.out.println(powerN(3, 2)); // 9
+        System.out.println(powerN(3, 3)); // 27
+    }
+
+    /*
+     *Given base and n that are both 1 or more,
+     compute recursively (no loops) the value of
+     base to the n power, so powerN(3, 2) is 9 (3 squared).
+     */
+
+     public static int powerN(int base, int n) {
+       if (n == 0) {
+         return 1;
+       } else {
+         return base * powerN(base, n-1);
+       }
+}
+}
