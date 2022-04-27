4ba325a763dd5935cb5e6f0a147979134e5c9179
Alif Rahman <arahman30@stuy.edu>
2022-04-27 14:37:24 -0400

    lab

---
+++ cobo/celeb/SimpleGUIRunner.java
@@ -0,0 +1,19 @@
+/*
+TNPG: Flying Sullen Actors (Alif Rahman, Salaj Rijal, Faiyaz Rafee)
+APCS
+L09: Some Folks Call It A Charades
+2022-04-27
+time spent: 4 hours
+*/
+
+import javax.swing.JFrame;
+
+public class SimpleGUIRunner
+{
+	public static void main(String[] args)
+	{
+		JFrame sample = new JFrame();
+		sample.setVisible(true);
+	}
+
+}
